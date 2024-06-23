788ef9ae3e26926599b57d587447c70b4cecb745
ShoshiMishkovski <hand1c22@mbjcomp.org.il>
2024-06-23 12:05:47 +0300

    add functions

--- userLoginBackend/Project/Server/Controllers/UsersController.cs
+++ userLoginBackend/Project/Server/Controllers/UsersController.cs
@@ -18,8 +18,23 @@ namespace userLoginBackend.Controllers
         public ActionResult<List<User>> GetAll()
         {
             return Ok(user.GetAll());
-            
-        }
 
+        }
+        [HttpPost("forgot-password")]
+        public async Task<IActionResult> ForgotPassword([FromBody] ForgotPasswordRequest request)
+        {
+            //var result = await user.SendPasswordResetLink(request.Email);
+            //if (result)
+            //{
+            //    return Ok(new { message = "Password reset link sent." });
+            //}
+            return BadRequest(new { message = "User not found." });
+        }
     }
+
+    public class ForgotPasswordRequest
+    {
+        public string Email { get; set; }
+    }
+
 }
