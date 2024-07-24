181c095183be37779b342eea87dbb9dc8e4adbe6
אלישבע רחל פישר <elishevarachel@office.eduil.org>
2024-07-24 16:04:10 +0300

    aaa

--- userLoginBackend/Project/BL/models_bll/BorrowApprovalRequestModelBLL.cs
+++ userLoginBackend/Project/BL/models_bll/BorrowApprovalRequestModelBLL.cs
@@ -8,10 +8,20 @@ namespace BLL.models_bll
     public class BorrowApprovalRequestModelBLL
     {
         public int searchDate { get; set; }
-        public int ItemId { get; set; }
+
+        public int RequestId { get; set; }
+
+        public int? ItemId { get; set; }
+
         public int UserId { get; set; }
+
         public int RequestStatus { get; set; }
+
         public DateTime RequestDate { get; set; }
+
         public DateTime? ApprovalDate { get; set; }
+
+        public int LibrariansId { get; set; }
+
     }
 }
